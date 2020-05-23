c47c75025b91d472ae06cae53411655224c0aed6
jiangqn <1935448858@qq.com>
2020-05-23 23:43:49 +0800

    update

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from nmt.training import train
 import os
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
-config_path = 'configs/transformer_en_zh.yaml'
+config_path = 'configs/transformer_en_zh2.yaml'
 
 train(config_path)
